8c5fad02618973974f4ae0e71f67817c1fbccba7
lillian <keynesiane@gmail.com>
2025-02-20 15:15:38 -0800

    add prisma session

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 /////// app.js
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 const { PrismaClient } = require('@prisma/client');
 const path = require("node:path");
 const express = require("express");
-const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require("bcryptjs");
@@ -21,7 +22,25 @@ const prisma = new PrismaClient()
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(
+  expressSession({
+    cookie: {
+     maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+    },
+    secret: 'a santa at nasa',
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+      new PrismaClient(),
+      {
+        checkPeriod: 2 * 60 * 1000,  //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  })
+);
+
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
