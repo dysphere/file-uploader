cf718fc84da34dcb539391e8cfcb3a4c1cb308f1
lillian <keynesiane@gmail.com>
2025-02-22 09:57:56 -0800

    change forms

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -3,4 +3,11 @@ const prisma = require("../prisma/prisma")
 exports.folder_create_get = (req, res, next) => 
     res.render("create_folder", { user: req.user, title: "Sign Up Form"});
 
-exports.folder_create_post = async (req, res, next) => {};
+exports.folder_create_post = async (req, res, next) => {
+    /*
+    await prisma.folder.create({
+        data: {
+          
+        },
+      }); */
+};
