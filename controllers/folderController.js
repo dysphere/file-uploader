bf7e9058b88b410565200eba19b25763cf07cffe
lillian <keynesiane@gmail.com>
2025-02-24 14:58:28 -0800

    mass delete fn and file upload form view

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,8 +1,8 @@
 const prisma = require("../db/prisma");
-const asyncHandler = require("express-async-handler");
 
-exports.folder_create_get = async (req, res, next) => 
+exports.folder_create_get = async (req, res, next) => {
     res.render("create_folder", { user: req.user, title: "Sign Up Form"});
+}
 
 
 exports.folder_create_post = async (req, res, next) => {
@@ -34,12 +34,45 @@ exports.folder_read_get = async (req, res, next) => {
     res.render("folder_view", {folder: folder});
 }
 
-exports.folder_read_post = (req, res, next) => {}
+exports.folder_update_get = async (req, res, next) => {
+  const folder = await prisma.folder.findUnique({
+        where: {
+          id: parseInt(req.params.id),
+        },
+      });
+  res.render("update_folder", {user: req.user, folder: folder});
+}
 
-exports.folder_update_get = (req, res, next) => {}
+exports.folder_update_post = async (req, res, next) => {
+  try {
+    await prisma.folder.update({
+      where: {
+        id: parseInt(req.params.id),
+      },
+      data: {
+        name: req.body.name,
+        userId: req.user.id,
+      },
+    })
+    res.redirect(`/folder/${folder.id}`);
+   } catch (error) {
+      console.error(error);
+      next(error);
+     }
+}
 
-exports.folder_update_post = (req, res, next) => {}
 
-exports.folder_delete_get = (req, res, next) => {}
+exports.folder_delete_post = async (req, res, next) => {
+  await prisma.folder.delete({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+  res.redirect("/");
+}
 
-exports.folder_delete_post = (req, res, next) => {}
+exports.folders_delete = async (req, res, next) => {
+  await prisma.folder.deleteMany({});
+  await prisma.$executeRaw`ALTER SEQUENCE "folder_id_seq" RESTART WITH 1;`; // PostgreSQL
+  res.redirect("/");
+}
--- routes/folders.js
+++ routes/folders.js
@@ -7,12 +7,12 @@ router.get('/create', folder_controller.folder_create_get);
 router.post('/create', folder_controller.folder_create_post);
 
 router.get('/:id', folder_controller.folder_read_get);
-router.post('/:id', folder_controller.folder_read_post);
 
 router.get('/:id/update', folder_controller.folder_update_get);
 router.post('/:id/update', folder_controller.folder_update_post);
 
-router.get('/:id/delete', folder_controller.folder_delete_get);
 router.post('/:id/delete', folder_controller.folder_delete_post);
+
+router.post("/delete", folder_controller.folders_delete);
 
 module.exports = router; 
